6bd9f5e93b5f4ced027bd08cdffed05ff8062fb6
Larry Cessna <larry.cessna@outlook.com>
2017-05-25 21:08:01 -0700

    removed alerts after every answer and added var correctAnswers to keep score

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var confirmed = confirm('Are you ready to rock and roll?');
+var correctAnswer = 5;
 
 if(confirmed === true){
   console.log('The user is ready!');
@@ -8,22 +9,22 @@ if(confirmed === true){
   console.log('The user is not ready...but we will start anyway!');
 }
 
-var firstAnswer = prompt('All questions should be answered with a Y or N. Is Larry Married?');
+var firstAnswer = prompt('All questions should be answered with a Y or N. \n Is Larry Married?');
 
 if (firstAnswer.toUpperCase() === 'Y') {
-  alert('Yes, you are correct!');
-  //console.log('Yes, you are correct!');
-} else{
-  alert('Sorry ladies. He\'s taken.');
-  //console.log('Sorry ladies. He\'s taken.');
+  console.log('Yes, you are correct!');
+} else {
+  correctAnswer = correctAnswer - 1;
+  console.log('Sorry ladies. He\'s taken.');
 }
 
 var secondAnswer = prompt('Does Larry have children?');
 
 if (secondAnswer.toUpperCase() === 'Y') {
-  console.log('Right again! They are awesome people. As you can see.');
+  console.log('Right! They are awesome people.');
 } else {
-  console.log('Wrong. Try again.')
+  correctAnswer = correctAnswer - 1;
+  console.log('Wrong. Try again.');
 }
 
 var thirdAnswer = prompt('Does Larry play the guitar?');
@@ -31,12 +32,14 @@ var thirdAnswer = prompt('Does Larry play the guitar?');
 if (thirdAnswer.toUpperCase() === 'Y') {
   console.log('Correct. He doesn\'t play well necessarily, but he does play');
 } else {
+  correctAnswer = correctAnswer - 1;
   console.log('Wrong answer! Do you even know him at all?');
 }
 
 var fourthAnswer = prompt('Does Larry have a cat?');
 
 if (fourthAnswer.toUpperCase() === 'Y') {
+  correctAnswer = correctAnswer - 1;
   console.log('Hell to the no!');
 } else {
   console.log('Dogs! All day everyday!');
@@ -49,3 +52,6 @@ if (fifthAnswer.toUpperCase() === 'Y') {
 } else {
   console.log('Go Navy! Beat Army!');
 }
+document.write('Larry is Married, he has children, he plays the guitar, he has a dog, and Retired from the Navy in 2015.');
+
+alert('You got ' + correctAnswer + ' out of 5 correct!');
