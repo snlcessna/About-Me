79ed5ca1d52f8fe367e875b08a21ca7b77a892ae
Larry Cessna <larry.cessna@outlook.com>
2017-06-03 13:16:05 -0700

    Added question 6 from Lab 3, added userName, and made minor edits

--- app.js
+++ app.js
@@ -1,58 +1,76 @@
 'use strict';
 
-var confirmed = confirm('Are you ready to rock and roll?');
-var correctAnswer = 5;
+var confirmed = confirm('Ready to get started?');
+var correctAnswer = ''; // Declared bucket to place number of correct answers
+
+var userName = prompt('Please provide your name. Don\'t worry, I\'ll wait.'); // Placeholder for the person playing.
 
 if(confirmed === true){
-  console.log('The user is ready!');
+  console.log(userName + ' is ready!');
 } else {
-  console.log('The user is not ready...but we will start anyway!');
+  console.log(userName + ' says they\'re not ready...but we will start anyway!');
 }
 
 var firstAnswer = prompt('All questions should be answered with a Y or N. \n Is Larry Married?');
 
 if (firstAnswer.toUpperCase() === 'Y') {
-  console.log('Yes, you are correct!');
+  correctAnswer++;
+  console.log('Larry is married, correct.');
 } else {
-  correctAnswer = correctAnswer - 1;
   console.log('Sorry ladies. He\'s taken.');
 }
 
 var secondAnswer = prompt('Does Larry have children?');
 
 if (secondAnswer.toUpperCase() === 'Y') {
-  console.log('Right! They are awesome people.');
+  correctAnswer++;
+  console.log('Right! And they are awesome people.');
 } else {
-  correctAnswer = correctAnswer - 1;
   console.log('Wrong. Try again.');
 }
 
 var thirdAnswer = prompt('Does Larry play the guitar?');
 
 if (thirdAnswer.toUpperCase() === 'Y') {
+  correctAnswer++;
   console.log('Correct. He doesn\'t play well necessarily, but he does play');
 } else {
-  correctAnswer = correctAnswer - 1;
   console.log('Wrong answer! Do you even know him at all?');
-} 
+}
 
 var fourthAnswer = prompt('Does Larry have a cat?');
 
 if (fourthAnswer.toUpperCase() === 'Y') {
-  correctAnswer = correctAnswer - 1;
   console.log('Hell to the no!');
 } else {
+  correctAnswer++;
   console.log('Dogs! All day everyday!');
 }
 
 var fifthAnswer = prompt('Was Larry in the Army?');
 
 if (fifthAnswer.toUpperCase() === 'Y') {
-  correctAnswer = correctAnswer - 1;
   console.log('Nope!');
 } else {
+  correctAnswer++;
   console.log('Go Navy! Beat Army!');
 }
-document.write('Larry is Married, he has children, he plays the guitar, he has a dog, and Retired from the Navy in 2015.');
+/*
+var sixthAnswer = prompt('Can you guess my favorite number?');
 
-alert('You got ' + correctAnswer + ' out of 5 correct!');
+if (sixthAnswer != '7') {
+  console.log('Try again');
+} else {
+  correctAnswer++;
+  console.log('You got it!');
+}
+*/
+document.write('Larry is Married, he has children, he plays the guitar, he has a dog, and Retired from the Navy in 2015.');
+/*
+if (correctAnswer === "7") {
+  alert('Wow! You got all of the answers correct!')
+} elseif (correctAnswer === "6" || correctAnswer === "5" ||correctAnswer === "4" ) {
+  alert ('You got ' + correctAnswer + ' out of 7. Not bad!')
+} else {
+alert('You probably should go grab a beer with Larry. Because you only got ' + correctAnswer + ' out of 6 correct.');
+}*/
